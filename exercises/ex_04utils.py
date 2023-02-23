0c44a6737f9d39768e1109a9f2b7b5e3fd8e8280
Alexander Vierling <zcv@live.unc.edu>
2023-02-23 11:24:30 -0500

    Progress on Exercise 4

---
+++ exercises/ex_04utils.py
@@ -0,0 +1,44 @@
+"""Establishes a few fuctions that are inherent in python to better understand lists. """
+
+__author__= "730308740"
+
+def all(numbers: list[int], comparison: int) -> bool: 
+    """If all the numbers in a list are the same as the comparison value it returns true."""
+
+    length_list: int = len(numbers)
+    i = 0 
+    while i < length_list: 
+        if comparison != numbers[i]: 
+            return False
+        i += 1   
+    return True 
+  
+def max(max_list: list[int]) -> int: 
+    """Gives you the max of a list of numbers."""
+
+    if len(max_list) == 0: 
+        raise ValueError("max() arg is an empty list")   
+
+    current_max: int = max_list[0]
+    i = 1 
+    while i < len(max_list): 
+        if current_max < max_list[i]: 
+                current_max = max_list[i] 
+        i +=1 
+    return(current_max)
+
+def is_equal(input1: list[int], input2: list[int]) -> bool: 
+    if len(input1) != len(input2): 
+       return False 
+     
+    i = 0 
+    while i < len(input1): 
+          if input1[i] != input2[i]: 
+               return False 
+          i += 1
+    return True
+        
+print(is_equal([1,9,3,4], [1,2,3,4]))
+       
+        
+
