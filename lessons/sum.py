fd969217b9cc8cd67b4c55c08d378f96c54629ae
Alexander Vierling <zcv@live.unc.edu>
2023-03-02 18:46:47 -0500

    unit tests exercises

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,6 +1,6 @@
 """This sums a list."""
 
-def sum(xs: list[float]) -> float: 
+def sum_old(xs: list[float]) -> float: 
     """return sum of elements in a list"""
     sum_total = 0.0 
     for value in xs: 
@@ -8,3 +8,10 @@ def sum(xs: list[float]) -> float:
     return sum_total
 
 
+def sum(xs: list[float]) -> float: 
+    """return sum of elements in a list"""
+    sum_total = 0.0 
+    idx = 0
+    for idx in range(0,len(xs),1): 
+        sum_total += xs[idx]
+    return sum_total
